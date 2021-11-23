498b195e9e192ee2ceb9204c88e1aea5b47bd609
The Beefy Mang <jesus.rosa75@yahoo.com>
2021-11-22 21:49:54 -0500

    password script written

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+"]
+var num = ["0", "1","2","3","4","5", "6", "7", "8", "9"]
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword(
+function generatePassword() {
+  var characterLength = parseInt(prompt("Enter a password between 8 and 128 character."));
+  var characterUpper = confirm("Would you like Uppercase characters?");
+  var characterLower = confirm("Would you like lowercase characters?");
+  var characterSpecial = confirm("Would you like special characters?");
+  var characterNum = confirm("Would you like number characters?");
+  var possibleCharacters = [];
+  var placeHolder = [];
 
-  );
+  if (characterLength >= 8 && characterLength <= 128) {      
+  } 
+
+  if (characterUpper) {  
+    possibleCharacters = possibleCharacters.concat(upper)  
+  }
+
+  if (characterLower) {   
+    possibleCharacters = possibleCharacters.concat(lower) 
+  }
+
+  if (characterSpecial) {
+    possibleCharacters = possibleCharacters.concat(special)
+  }
+
+  if (characterNum) {    
+    possibleCharacters = possibleCharacters.concat(num)
+  }
+
+  for(var i = 0; i < characterLength; i++){
+   var randomNum = Math.floor(Math.random() * possibleCharacters.length) + 0;
+   placeHolder.push(possibleCharacters[randomNum]);   
+  }
+
+  
+  // empty if statement if all character are false
+
+
+  return placeHolder.join("");
+}
+
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
