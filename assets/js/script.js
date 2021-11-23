96ca1ba072b5616debb7619de8f7152bdd41d889
The Beefy Mang <jesus.rosa75@yahoo.com>
2021-11-23 09:51:56 -0500

    some changes to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+"]
-var num = ["0", "1","2","3","4","5", "6", "7", "8", "9"]
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 // Write password to the #password input
 function generatePassword() {
@@ -15,32 +15,35 @@ function generatePassword() {
   var possibleCharacters = [];
   var placeHolder = [];
 
-  if (characterLength >= 8 && characterLength <= 128) {      
-  } 
+  if (characterLength >= 8 && characterLength <= 128) {
 
-  if (characterUpper) {  
-    possibleCharacters = possibleCharacters.concat(upper)  
-  }
+    if (characterUpper) {
+      possibleCharacters = possibleCharacters.concat(upper)
+    }
 
-  if (characterLower) {   
-    possibleCharacters = possibleCharacters.concat(lower) 
-  }
+    if (characterLower) {
+      possibleCharacters = possibleCharacters.concat(lower)
+    }
 
-  if (characterSpecial) {
-    possibleCharacters = possibleCharacters.concat(special)
-  }
+    if (characterSpecial) {
+      possibleCharacters = possibleCharacters.concat(special)
+    }
 
-  if (characterNum) {    
-    possibleCharacters = possibleCharacters.concat(num)
-  }
+    if (characterNum) {
+      possibleCharacters = possibleCharacters.concat(num)
+    }
 
-  for(var i = 0; i < characterLength; i++){
-   var randomNum = Math.floor(Math.random() * possibleCharacters.length) + 0;
-   placeHolder.push(possibleCharacters[randomNum]);   
+    // empty if statement if all character are false
+
+    for (var i = 0; i < characterLength; i++) {
+      var randomNum = Math.floor(Math.random() * possibleCharacters.length) + 0;
+      placeHolder.push(possibleCharacters[randomNum]);
+    }
+
   }
 
   
-  // empty if statement if all character are false
+
 
 
   return placeHolder.join("");
